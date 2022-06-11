dc04a9109ec2159a80949ca641de3ef9a3e09993
Daryl Manning <daryl@wakatara.com>
2022-06-11 15:48:47 +0800

    Player rendering via pixel sheet sorted.

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 const (
 	screenWidth  = 1000
@@ -11,9 +11,18 @@ const (
 var (
 	running  = true
 	bkgColor = rl.NewColor(147, 211, 196, 255)
+
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+
+	playerSrc  rl.Rectangle
+	playerDest rl.Rectangle
 )
 
-func drawScene() {}
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
 
 func input() {
 
@@ -37,9 +46,18 @@ func init() {
 	rl.InitWindow(screenWidth, screenHeight, "Sproutlings!")
 	rl.SetExitKey(0)
 	rl.SetTargetFPS(fps)
+
+	grassSprite = rl.LoadTexture("res/SproutLands/Tilesets/grass.png")
+	playerSprite = rl.LoadTexture("res/SproutLands/characters/Basic Charakter Spritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
 }
 
 func quit() {
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
 	rl.CloseWindow()
 }
 
