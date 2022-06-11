fe94da2468d88d69ea7daad2b561b4d9a4e7cb53
Daryl Manning <daryl@wakatara.com>
2022-06-11 15:06:31 +0800

    Basic window and loop config sorted.

--- main.go
+++ main.go
@@ -2,31 +2,54 @@ package main
 
 import "github.com/gen2brain/raylib-go/raylib"
 
+const (
+	screenWidth  = 1000
+	screenHeight = 480
+	fps          = 60
+)
+
+var (
+	running  = true
+	bkgColor = rl.NewColor(147, 211, 196, 255)
+)
+
+func drawScene() {}
+
 func input() {
 
 }
 
 func update() {
+	running = !rl.WindowShouldClose()
 
 }
 
 func render() {
-
 	rl.BeginDrawing()
-	rl.ClearBackground(rl.RayWhite)
+	rl.ClearBackground(bkgColor)
+
+	drawScene()
+
 	rl.EndDrawing()
 }
 
+func init() {
+	rl.InitWindow(screenWidth, screenHeight, "Sproutlings!")
+	rl.SetExitKey(0)
+	rl.SetTargetFPS(fps)
+}
+
+func quit() {
+	rl.CloseWindow()
+}
+
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 
-	rl.SetTargetFPS(60)
-
-	for !rl.WindowShouldClose() {
+	for running {
 		input()
 		update()
 		render()
 	}
 
-	rl.CloseWindow()
+	quit()
 }
